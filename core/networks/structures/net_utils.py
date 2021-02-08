c327c23bc9ec5b13baf66d6ed689248368bc3ab3
YutouTaro <kxhyu17@gmail.com>
2021-02-08 16:52:26 +0800

    added the shape of some tensor/array as comment

--- core/networks/structures/net_utils.py
+++ core/networks/structures/net_utils.py
@@ -28,9 +28,9 @@ def warp_flow(x, flow, use_mask=False):
     # mesh grid 
     xx = torch.arange(0, W).view(1,-1).repeat(H,1)
     yy = torch.arange(0, H).view(-1,1).repeat(1,W)
-    xx = xx.view(1,1,H,W).repeat(B,1,1,1)
-    yy = yy.view(1,1,H,W).repeat(B,1,1,1)
-    grid = torch.cat((xx,yy),1).float()
+    xx = xx.view(1,1,H,W).repeat(B,1,1,1) # B, 1, H, W
+    yy = yy.view(1,1,H,W).repeat(B,1,1,1) # B, 1, H, W
+    grid = torch.cat((xx,yy),1).float() # B, 2, H, W
 
     if grid.shape != flow.shape:
         raise ValueError('the shape of grid {0} is not equal to the shape of flow {1}.'.format(grid.shape, flow.shape))
@@ -42,7 +42,7 @@ def warp_flow(x, flow, use_mask=False):
     vgrid[:,0,:,:] = 2.0*vgrid[:,0,:,:].clone() / max(W-1,1)-1.0
     vgrid[:,1,:,:] = 2.0*vgrid[:,1,:,:].clone() / max(H-1,1)-1.0
 
-    vgrid = vgrid.permute(0,2,3,1)        
+    vgrid = vgrid.permute(0,2,3,1) # B, H, W, 2
     output = nn.functional.grid_sample(x, vgrid)
     if use_mask:
         mask = torch.autograd.Variable(torch.ones(x.size())).to(x.get_device())
